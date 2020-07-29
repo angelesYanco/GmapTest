91c84fd36b1769f77bdfa38c9fb302d1c26fc4c6
apangeles <apena@porrua.com>
2020-07-28 19:24:11 -0500

    Starting project

---
+++ GmapTest.WinForms/frmGMaps.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GmapTest.WinForms
+{
+    public partial class frmGMaps : Form
+    {
+        public frmGMaps()
+        {
+            InitializeComponent();
+        }
+    }
+}
